3db278b55049593e70c05fdbdf988d2b23168516
Ben <benharvie94@gmail.com>
2018-09-05 20:22:18 +0100

    5/7 tests passing

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -3,7 +3,25 @@ const AnagramFinder = function (word) {
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
+  //check length = word
+  //downcase
+  let anagramsFound = []
+  let anagram = true
+  letters = this.word.split('');
+  console.log(this.word,letters);
+  otherWords.forEach(word => {
+    letters.forEach(letter => {
+      if (!word.includes(letter)) {
+        anagram = false
+      }
+    })
+    if (anagram === true && anagram.length === word.length) {
+      anagramsFound.push(word);
+    }
+  })
 
+
+  return anagramsFound;
 }
 
 module.exports = AnagramFinder;
