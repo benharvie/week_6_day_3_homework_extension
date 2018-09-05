681e583f030f18c9c0b1ec905161158738319490
Ben <benharvie94@gmail.com>
2018-09-05 20:05:41 +0100

    panagram finder complete

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,16 @@
 const PangramFinder = function (phrase) {
+  this.phrase = phrase;
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  let pangram = true
+  this.alphabet.forEach(letter => {
+    if (!this.phrase.toLowerCase().includes(letter)) {
+      pangram = false
+    }
+  })
+  return pangram
 }
 
 module.exports = PangramFinder;
