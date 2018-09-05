d84691aae7c7e0a41ba67c8e92df5dfe3acad18d
Ben <benharvie94@gmail.com>
2018-09-05 21:05:34 +0100

    isogram finder complete

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,26 @@
 const IsogramFinder = function (word) {
-
+  this.word = word;
 }
 
-IsogramFinder.prototype.isIsogram = function () {
+// An isogram is a logological term for a word or phrase without
+// a repeating letter. It is also used by some to mean a word or
+// phrase in which each letter appears the same number of times,
+// not necessarily just once.
 
+IsogramFinder.prototype.isIsogram = function () {
+  let result = true
+  let letterFound = {}
+  let letters = this.word.toUpperCase().split('');
+  letters.forEach(letter => {
+    if (!letterFound[letter]) {
+      letterFound[letter] = true;
+    }
+    else {
+      finalResult = false;
+      letterFound[letter] = false;
+    }
+  })
+  return result;
 }
 
 module.exports = IsogramFinder;
